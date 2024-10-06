9dc54c5891bd6bfee53e51a551b92ecf7a6c1d03
ankit buade <ankitbuade20338@acropolis.in>
2024-10-06 14:54:34 +0530

    Installed MondoDB and did some basic operation

---
+++ Database.js
@@ -0,0 +1,13 @@
+// const URI ="mongodb+srv://BackendLearning:4XTKei1s2BW9nMEb@learningbackend.je8ms.mongodb.net/?retryWrites=true&w=majority&appName=LearningBackend"
+
+// const URI = "mongodb+srv://BackendLearning:4XTKei1s2BW9nMEb@learningbackend.je8ms.mongodb.net/"
+
+// const URI = "mongodb+srv://LearningBackend:4XTKei1s2BW9nMEb@learningbackend.je8ms.mongodb.net/?retryWrites=true&w=majority&appName=LearningBackend"
+
+//  ankitpawar
+
+// const URI = mongodb+srv://<db_username>:<db_password>@backend.20hhu.mongodb.net/?retryWrites=true&w=majority&appName=Backend
+
+const {MongoClient } =require ("mongodb")
+
+const URI ="mongodb+srv://BackendpWN4kAQ6e5bXegbe@backend.20hhu.mongodb.net/"
